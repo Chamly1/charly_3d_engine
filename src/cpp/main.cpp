c42b0644f9d4f7508f0723bd9cfde3259dcb2c83
Chamly <vladskotarenko6227@gmail.com>
2023-07-21 19:12:10 +0300

    add window creation

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,6 +1,61 @@
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
 #include <iostream>
+
+// window size
+static const GLint WIDTH = 800, HEIGHT = 600;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    if (!glfwInit()) {
+        std::cout << "GLFW initialization failed!\n";
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // no backwards compatibility?
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow *mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "title", NULL, NULL);
+    if (!mainWindow) {
+        std::cout << "GLFW window creation failed!\n";
+        glfwTerminate();
+        return 1;
+    }
+
+    int bufferWidth, bufferHeight;
+    glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+    // set context for GLEW to use
+    glfwMakeContextCurrent(mainWindow);
+
+    glewExperimental = GL_TRUE;
+
+    if (glewInit() != GLEW_OK) {
+        std::cout << "GLEW initialization failed!\n";
+        glfwDestroyWindow(mainWindow);
+        glfwTerminate();
+        return 1;
+    }
+
+    // setup viewport size
+    glViewport(0, 0, bufferWidth, bufferHeight);
+
+    while (!glfwWindowShouldClose(mainWindow)) {
+        glfwPollEvents();
+
+        glClearColor(0.f, 1.f, 0.f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(mainWindow);
+    }
+
+    glfwDestroyWindow(mainWindow);
+    glfwTerminate();
+
     return 0;
 }
