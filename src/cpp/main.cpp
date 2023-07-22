d3d172bf3bd15fb2d653224e63d08a067c40080a
Chamly <vladskotarenko6227@gmail.com>
2023-07-22 13:21:58 +0300

    add a basic triangle drawing

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -6,6 +6,89 @@
 // window size
 static const GLint WIDTH = 800, HEIGHT = 600;
 
+GLuint VAO, VBO, shader;
+
+// vertex shader
+static const char* vertexShader = "#version 330\n layout (location = 0) in vec3 pos; void main() {gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);}";
+// fragment shader
+static const char* fragmentShader = "#version 330\n out vec4 color; void main() {color = vec4(0.0, 1.0, 0.0, 1.0);}";
+
+void createTriangle() {
+    GLfloat vertices[] = {
+            -1.f, -1.f, 0.f,
+            1.f, -1.f, 0.f,
+            0.f, 1.f, 0.f
+    };
+
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+void addShader (GLuint program, const char* shaderCode, GLenum shaderType) {
+    GLuint shader = glCreateShader(shaderType);
+
+    const GLchar* code[1];
+    code[0] = shaderCode;
+
+    GLint codeLength[1];
+    codeLength[0] = strlen(shaderCode);
+
+    glShaderSource(shader, 1, code, codeLength);
+    glCompileShader(shader);
+
+    GLint res = 0;
+    GLchar log[1024] = { 0 };
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &res);
+    if (!res) {
+        glGetShaderInfoLog(shader, sizeof(log), NULL, log);
+        std::cout << "Error shader compiling: " << log << "\n";
+        return;
+    }
+
+    glAttachShader(program, shader);
+}
+
+void compileShaders () {
+    shader = glCreateProgram();
+    if (!shader) {
+        std::cout << "Create shader program error!\n";
+        return;
+    }
+
+    addShader(shader, vertexShader, GL_VERTEX_SHADER);
+    addShader(shader, fragmentShader, GL_FRAGMENT_SHADER);
+
+    GLint res = 0;
+    GLchar log[1024] = { 0 };
+
+    glLinkProgram(shader);
+    glGetProgramiv(shader, GL_LINK_STATUS, &res);
+    if (!res) {
+        glGetProgramInfoLog(shader, sizeof(log), NULL, log);
+        std::cout << "Error linking program: " << log << "\n";
+        return;
+    }
+
+    glValidateProgram(shader);
+    glGetProgramiv(shader, GL_VALIDATE_STATUS, &res);
+    if (!res) {
+        glGetProgramInfoLog(shader, sizeof(log), NULL, log);
+        std::cout << "Error validation program: " << log << "\n";
+        return;
+    }
+
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
@@ -45,11 +128,20 @@ int main() {
     // setup viewport size
     glViewport(0, 0, bufferWidth, bufferHeight);
 
+    createTriangle();
+    compileShaders();
+
     while (!glfwWindowShouldClose(mainWindow)) {
         glfwPollEvents();
 
-        glClearColor(0.f, 1.f, 0.f, 1.f);
+        glClearColor(0.f, 0.f, 0.f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+        glUseProgram(0);
 
         glfwSwapBuffers(mainWindow);
     }
