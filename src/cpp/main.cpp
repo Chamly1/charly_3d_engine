ca9ae8a32d7740d0105e5093bfa0f73ae4290ccf
Chamly <vladskotarenko6227@gmail.com>
2023-07-24 00:13:49 +0300

    add depth test

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -162,6 +162,7 @@ int main() {
         return 1;
     }
 
+    glEnable(GL_DEPTH_TEST);
     // setup viewport size
     glViewport(0, 0, bufferWidth, bufferHeight);
 
@@ -191,7 +192,7 @@ int main() {
         glfwPollEvents();
 
         glClearColor(0.f, 0.f, 0.f, 1.f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glUseProgram(shader);
 
