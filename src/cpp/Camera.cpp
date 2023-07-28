b668b2f6a99f0fde5a986f3d553ccb70f4b8be5d
Chamly <vladskotarenko6227@gmail.com>
2023-07-28 03:01:11 +0300

    add base Camera class implementation

---
+++ src/cpp/Camera.cpp
@@ -0,0 +1,60 @@
+#include "Camera.hpp"
+
+Camera::Camera(glm::vec3 position, glm::vec3 worldUp, GLfloat yaw, GLfloat pitch)
+: mPosition(position)
+, mWorldUp(worldUp)
+, mYaw(yaw)
+, mPitch(pitch)
+, mFront(0.f, 0.f, -1.f)
+, mMoveSpeed(0.1f)
+, mTurnSpeed(0.1f)
+, mMoveDirection(0.f) {
+
+}
+
+void Camera::update() {
+    // update rotation
+    mFront.x = cos(glm::radians(mYaw)) * cos(glm::radians(mPitch));
+    mFront.y = sin(glm::radians(mPitch));
+    mFront.z = sin(glm::radians(mYaw)) * cos(glm::radians(mPitch));
+    mFront = glm::normalize(mFront);
+
+    mRight = glm::normalize(glm::cross(mFront, mWorldUp));
+    mUp = glm::normalize(glm::cross(mRight, mFront));
+
+    // update movement
+    if (mMoveDirection.x != 0.f ||
+        mMoveDirection.x != 0.f ||
+        mMoveDirection.x != 0.f) {
+
+        mMoveDirection = glm::normalize(mMoveDirection);
+        mPosition += mMoveDirection * mMoveSpeed;
+        mMoveDirection = glm::vec3(0.f);
+    }
+
+}
+
+void Camera::move(MoveDirection direction) {
+    switch(direction) {
+        case MoveDirection::Forward:
+//            mPosition += mFront * mMoveSpeed;
+            mMoveDirection += mFront;
+            break;
+        case MoveDirection::Backward:
+//            mPosition -= mFront * mMoveSpeed;
+            mMoveDirection -= mFront;
+            break;
+        case MoveDirection::Left:
+//            mPosition -= mRight * mMoveSpeed;
+            mMoveDirection -= mRight;
+            break;
+        case MoveDirection::Right:
+//            mPosition += mRight * mMoveSpeed;
+            mMoveDirection += mRight;
+            break;
+    }
+}
+
+glm::mat4 Camera::calculateViewMatrix() {
+    return glm::lookAt(mPosition, mPosition + mFront, mUp);
+}
---
+++ src/include/Camera.hpp
@@ -0,0 +1,39 @@
+#ifndef LEARN_OPENGL_CAMERA_HPP
+#define LEARN_OPENGL_CAMERA_HPP
+
+#include "GL/glew.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+enum class MoveDirection {
+    Forward,
+    Backward,
+    Left,
+    Right
+};
+
+class Camera {
+private:
+    glm::vec3 mPosition;
+    glm::vec3 mFront;
+    glm::vec3 mUp;
+    glm::vec3 mRight;
+    glm::vec3 mWorldUp;
+
+    GLfloat mYaw;
+    GLfloat mPitch;
+
+    GLfloat mMoveSpeed;
+    GLfloat mTurnSpeed;
+
+    glm::vec3 mMoveDirection;
+
+public:
+    Camera(glm::vec3 position, glm::vec3 worldUp, GLfloat yaw, GLfloat pitch);
+
+    void update();
+    void move(MoveDirection direction);
+    glm::mat4 calculateViewMatrix();
+};
+
+#endif //LEARN_OPENGL_CAMERA_HPP
