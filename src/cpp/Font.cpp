fc1f679d7f4b6cd9c48e681ca36e3d1e83f46dbc
Chamly <vladskotarenko6227@gmail.com>
2023-09-16 21:46:03 +0300

    add glyph atlas generation in Font

--- src/cpp/Font.cpp
+++ src/cpp/Font.cpp
@@ -6,7 +6,7 @@
 
 #include <iostream>
 
-static const unsigned int DEFAULT_FONT_SIZE = 1000;
+static const unsigned int DEFAULT_FONT_SIZE = 24;
 
 namespace Charly {
 
@@ -23,14 +23,51 @@ namespace Charly {
 //            return 1;
         }
 
-        FT_Set_Pixel_Sizes(face, 0, DEFAULT_FONT_SIZE);
-
         ///////////////////////////////////
 
-        if (FT_Load_Char(face, 'F', FT_LOAD_RENDER))
-        {
-            std::cout << "ERROR::FreeType: failed to load glyph\n";
-//            return 1;
+//        char firstChar = ' ';
+//        char lastChar = '~';
+        char firstChar = 'A';
+        char lastChar = 'Z';
+        int charCount = static_cast<int>(lastChar) - static_cast<int>(firstChar) + 1;
+        unsigned int fontSize = 24;
+        int atlasWidth = fontSize * charCount;
+        int atlasHeight = fontSize;
+
+        unsigned char* atlasData = new unsigned char[atlasWidth * atlasHeight * 4](); // RGBA format
+
+        int x = 0;
+        int y = 0;
+
+        FT_Set_Pixel_Sizes(face, 0, fontSize);
+        for (char c = firstChar; c <= lastChar; c++) {
+            if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
+                // Handle glyph loading error
+                continue;
+            }
+
+            FT_Bitmap glyphBitmap = face->glyph->bitmap;
+            int glyphWidth = glyphBitmap.width;
+            int glyphHeight = glyphBitmap.rows;
+
+//            std::cout << "glyphWidth: " << glyphWidth << ";\tglyphHeight: " << glyphHeight << '\n';
+
+            // Copy glyph bitmap data to the atlas image at position (x, y)
+            for (int i = 0; i < glyphHeight; i++) {
+                for (int j = 0; j < glyphWidth; j++) {
+//                    int atlasIndex = ((y + i) * atlasWidth + (x + j)) * 4;
+                    // fontSize - 1 - i added to flip atlas upside-down because OpenGL flips textures, but we want to UV's (0, 0) be at left bottom corner
+                    int atlasIndex = ((y + fontSize - 1 - i) * atlasWidth + (x + j)) * 4;
+                    int glyphIndex = i * glyphWidth + j;
+
+                    atlasData[atlasIndex + 0] = 255; // Red channel
+                    atlasData[atlasIndex + 1] = 255; // Green channel
+                    atlasData[atlasIndex + 2] = 255; // Blue channel
+                    atlasData[atlasIndex + 3] = glyphBitmap.buffer[glyphIndex]; // Alpha channel
+                }
+            }
+
+            x += fontSize;
         }
 
         glGenTextures(1, &mTexture);
@@ -38,13 +75,13 @@ namespace Charly {
         glTexImage2D(
                 GL_TEXTURE_2D,
                 0,
-                GL_RED,
-                face->glyph->bitmap.width,
-                face->glyph->bitmap.rows,
+                GL_RGBA,
+                atlasWidth,
+                atlasHeight,
                 0,
-                GL_RED,
+                GL_RGBA,
                 GL_UNSIGNED_BYTE,
-                face->glyph->bitmap.buffer
+                atlasData
         );
         // set texture options
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
@@ -58,8 +95,6 @@ namespace Charly {
 
 
 
-        ////////////////////////////
-
         float vertices[] = {
 //                0.0f, 0.0f, 0.0f, 0.0f,
 //                0.0f, 1.0f, 0.0f, 1.0f,
@@ -69,13 +104,21 @@ namespace Charly {
 //                1.0f, 1.0f, 1.0f, 1.0f,
 //                1.0f, 0.0f, 1.0f, 0.0f
 
-                0.0f, 0.0f, 0.0f, 0.0f,
-                0.0f, 500.0f, 0.0f, 1.0f,
-                500.0f, 500.0f, 1.0f, 1.0f,
+//                0.0f, 0.0f, 0.0f, 0.0f,
+//                0.0f, 500.0f, 0.0f, 1.0f,
+//                500.0f, 500.0f, 1.0f, 1.0f,
+//
+//                0.0f, 0.0f, 0.0f, 0.0f,
+//                500.0f, 500.0f, 1.0f, 1.0f,
+//                500.0f, 0.0f, 1.0f, 0.0f
 
                 0.0f, 0.0f, 0.0f, 0.0f,
-                500.0f, 500.0f, 1.0f, 1.0f,
-                500.0f, 0.0f, 1.0f, 0.0f
+                0.0f, (float)atlasHeight, 0.0f, 1.0f,
+                (float)atlasWidth, (float)atlasHeight, 1.0f, 1.0f,
+
+                0.0f, 0.0f, 0.0f, 0.0f,
+                (float)atlasWidth, (float)atlasHeight, 1.0f, 1.0f,
+                (float)atlasWidth, 0.0f, 1.0f, 0.0f
         };
 
         BufferLayout bufferLayout = {
