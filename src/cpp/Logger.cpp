1779579f381539e9f11b721ce094aade0c328c82
Chamly <vladskotarenko6227@gmail.com>
2023-09-30 21:46:52 +0300

    add log macros

--- src/cpp/Logger.cpp
+++ src/cpp/Logger.cpp
@@ -1,11 +1,30 @@
 #include "Logger.hpp"
 
 #include <iostream>
+#include <chrono>
+#include <ctime>
 
 namespace Charly {
 
-    void Logger::log(const std::string& str) {
-        std::cout << str << std::endl;
+    void Logger::log(LogLevel logLevel, const std::string& file, int line, const std::string& message) {
+
+        // Get the current system time with milliseconds
+        auto now = std::chrono::system_clock::now();
+        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
+
+        // Convert the milliseconds to a time_point
+        std::chrono::system_clock::time_point tp = std::chrono::system_clock::time_point(std::chrono::milliseconds(ms));
+
+        // Convert the time_point to a time_t (seconds since epoch)
+        std::time_t time = std::chrono::system_clock::to_time_t(tp);
+
+        // Format the time with milliseconds
+        std::tm tm = *std::localtime(&time);
+        char buffer[80];
+        strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S.", &tm);
+
+        // Print the formatted time with milliseconds
+        std::cout << "[" << buffer << ms % 1000 << "] [" << getLogLevelStr(logLevel) << "] [" << file << ":" << line << "] " << message << std::endl;
     }
 
 }
--- src/include/Logger.hpp
+++ src/include/Logger.hpp
@@ -5,23 +5,45 @@
 
 #ifdef ENABLE_LOGGING
 
-#define LOG(str) \
-    Charly::Logger::log(str);
+#define LOG_ERROR(str) \
+    Charly::Logger::log(Charly::LogLevel::Error, __FILE__, __LINE__, str);
+
+#define LOG_WARNING(str) \
+    Charly::Logger::log(Charly::LogLevel::Warning, __FILE__, __LINE__, str);
 
 #else
 
-#define LOG(str)
+#define LOG_ERROR(str)
+#define LOG_WARNING(str)
 
 #endif
 
+// target log format: [YYYY-MM-DD HH:MM:SS.MMM] [LogLevel] [Module] [File:Line] Message
+
 namespace Charly {
+
+    enum class LogLevel {
+        Error, Warning, Debug, Info, Fatal
+    };
+
+    inline const char* getLogLevelStr(LogLevel logLevel) {
+        switch (logLevel) {
+            case LogLevel::Error:       return "ERROR";
+            case LogLevel::Warning:     return "WARNING";
+            case LogLevel::Debug:       return "DEBUG";
+            case LogLevel::Info:        return "INFO";
+            case LogLevel::Fatal:       return "FATAL";
+        }
+
+        return "";
+    }
 
     class Logger {
     private:
 
 
     public:
-        static void log(const std::string& str);
+        static void log(LogLevel logLevel, const std::string& file, int line, const std::string& message);
 
         // delete implicit methods
         Logger(Logger const &) = delete;
